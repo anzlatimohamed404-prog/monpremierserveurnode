997d23126bc159787c72d9faa0b99ceb059f1358
anzlatimohamed404-prog <anzlatimohamed404@gmail.com>
2026-01-26 11:02:50 +0300

    Integrer les middleware dans l'app.js

--- app.js
+++ app.js
@@ -6,9 +6,32 @@ const express = require('express');
 // J'initialise une application ExpressJs
 const app = express();
 
-app.use((req, res) => {
-    res.end("bonjour,je suis le serveur!");
+app.use((req, res, next) => {
+    console.log("bonjour,je suis le serveur!");
+    next();
 });
+
+app.use((req, res,next) => {
+    res.json({ message: "vous êtes bien sur le serveur Ndovoo" });
+    next();
+});
+
+app.use((req, res) => {
+    console.log("je suis dans le serveur!");
+    
+});
+
+// J'utilise un Middleware 2 pour retourner la réponse au client l'orsq'il fait la localhost:3000.
+// La réponse est au format JSON
+
+
+
+
+
+
+
+
+
 
 /*
 j'exporte l'application express sous forme de module
